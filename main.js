09471df2110c78adea78e0d403396606d81e6eda
Lucas Travagin <lucas_travagin1112@hotmail.com>
2020-06-03 13:14:26 -0300

    Subiuuu

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 const puppeter = require('puppeteer')
 const app = require('express')()
 const port = 3000
-const url = 'http://www.portaltransparencia.gov.br/pessoa-juridica/busca/lista'
 const _ = require('lodash')
 
 app.listen(port, function () {
@@ -15,13 +14,12 @@ app.listen(port, function () {
 //     defaultViewport: null
 // };
 
-
-
 app.get('/', async (req, res) => {
     try {
         var cnpj = req.query.cnpj
-        await scrape(cnpj).then((value)=>{
-            if (value == undefined) res.status(404).send({Erro: 'CNPJ não encontrado'})
+        const url = `http://www.portaltransparencia.gov.br/busca/pessoa-juridica/${cnpj}`
+        await scrape(url).then((value)=>{
+            if (value == undefined) res.status(404).send({Erro: 'CNPJ não encontrado no portal da transparência'})
             res.status(200).send(value)
         })
         
@@ -31,46 +29,33 @@ app.get('/', async (req, res) => {
 
 })
 
-let scrape = async (cnpj) => {
+let scrape = async (url) => {
     try {
         const browser = await puppeter.launch({args: ['--no-sandbox']})
         const page = await browser.newPage()
         await page.goto(url, { waitUntil: 'networkidle2' })
 
-        await page.type('#termo', cnpj)
 
-        await page.click('#btnBuscar')
-
-
-        await page.waitFor(2000)
-        await page.evaluate((browser) => {
-            if (document.getElementById('countResultados').textContent == "0") {
-                console.log('Deu erro')
-            } else {
-                document.querySelector("#resultados > li > h3 > a").click()
-            }
-        })
-
-
-        await page.waitFor(2000)
         const result = await page.evaluate(() => {
             let registros = {}
 
-            registros.cnpj = document.querySelector("body > main > div:nth-child(3) > section.dados-tabelados > div:nth-child(1) > div:nth-child(1) > span").textContent
-            registros.data_abertura = document.querySelector("body > main > div:nth-child(3) > section.dados-tabelados > div:nth-child(1) > div:nth-child(2) > span").textContent
-            registros.email = document.querySelector("body > main > div:nth-child(3) > section.dados-tabelados > div:nth-child(1) > div:nth-child(3) > span").textContent
-            registros.telefone = document.querySelector("body > main > div:nth-child(3) > section.dados-tabelados > div:nth-child(1) > div.col-xs-12.col-sm-2 > span").textContent
-            registros.razao_social = document.querySelector("body > main > div:nth-child(3) > section.dados-tabelados > div:nth-child(3) > div:nth-child(1) > span").textContent
-            registros.nome_fantasia = document.querySelector("body > main > div:nth-child(3) > section.dados-tabelados > div:nth-child(3) > div:nth-child(2) > span").textContent
-            registros.natureza_juridica = document.querySelector("body > main > div:nth-child(3) > section.dados-tabelados > div:nth-child(3) > div:nth-child(3) > span:nth-child(2)").textContent
-            registros.atividade_principal = document.querySelector("body > main > div:nth-child(3) > section.dados-tabelados > div:nth-child(3) > div:nth-child(4) > span").textContent
-            registros.logradouro = document.querySelector("body > main > div:nth-child(3) > section.dados-tabelados > div:nth-child(3) > div:nth-child(4) > span").textContent
-            registros.numero = document.querySelector("body > main > div:nth-child(3) > section.dados-tabelados > div:nth-child(5) > div:nth-child(2) > span").textContent
-            registros.complemento = document.querySelector("body > main > div:nth-child(3) > section.dados-tabelados > div:nth-child(5) > div:nth-child(3) > span").textContent
-            registros.cep = document.querySelector("body > main > div:nth-child(3) > section.dados-tabelados > div:nth-child(5) > div:nth-child(4) > span").textContent
-            registros.bairro = document.querySelector("body > main > div:nth-child(3) > section.dados-tabelados > div:nth-child(5) > div:nth-child(5) > span").textContent
-            registros.municipio = document.querySelector("body > main > div:nth-child(3) > section.dados-tabelados > div:nth-child(5) > div:nth-child(6) > span").textContent
-            registros.uf = document.querySelector("body > main > div:nth-child(3) > section.dados-tabelados > div:nth-child(5) > div:nth-child(7) > span").textContent
+            let cnpj = document.querySelector("body > main > div:nth-child(3) > section.dados-tabelados > div:nth-child(1) > div:nth-child(1) > span").innerText
+            registros.cnpj = cnpj.substring(0,18)
+            registros.tipo = cnpj.substring(19)
+            registros.data_abertura = document.querySelector("body > main > div:nth-child(3) > section.dados-tabelados > div:nth-child(1) > div:nth-child(2) > span").innerText
+            registros.email = document.querySelector("body > main > div:nth-child(3) > section.dados-tabelados > div:nth-child(1) > div:nth-child(3) > span").innerText
+            registros.telefone = document.querySelector("body > main > div:nth-child(3) > section.dados-tabelados > div:nth-child(1) > div.col-xs-12.col-sm-2 > span").innerText
+            registros.razao_social = document.querySelector("body > main > div:nth-child(3) > section.dados-tabelados > div:nth-child(3) > div:nth-child(1) > span").innerText
+            registros.nome_fantasia = document.querySelector("body > main > div:nth-child(3) > section.dados-tabelados > div:nth-child(3) > div:nth-child(2) > span").innerText
+            registros.natureza_juridica = document.querySelector("body > main > div:nth-child(3) > section.dados-tabelados > div:nth-child(3) > div:nth-child(3) > span:nth-child(2)").innerText
+            registros.atividade_principal = document.querySelector("body > main > div:nth-child(3) > section.dados-tabelados > div:nth-child(3) > div:nth-child(4) > span").innerText
+            registros.logradouro = document.querySelector("body > main > div:nth-child(3) > section.dados-tabelados > div:nth-child(3) > div:nth-child(4) > span").innerText
+            registros.numero = document.querySelector("body > main > div:nth-child(3) > section.dados-tabelados > div:nth-child(5) > div:nth-child(2) > span").innerText
+            registros.complemento = document.querySelector("body > main > div:nth-child(3) > section.dados-tabelados > div:nth-child(5) > div:nth-child(3) > span").innerText
+            registros.cep = document.querySelector("body > main > div:nth-child(3) > section.dados-tabelados > div:nth-child(5) > div:nth-child(4) > span").innerText
+            registros.bairro = document.querySelector("body > main > div:nth-child(3) > section.dados-tabelados > div:nth-child(5) > div:nth-child(5) > span").innerText
+            registros.municipio = document.querySelector("body > main > div:nth-child(3) > section.dados-tabelados > div:nth-child(5) > div:nth-child(6) > span").innerText
+            registros.uf = document.querySelector("body > main > div:nth-child(3) > section.dados-tabelados > div:nth-child(5) > div:nth-child(7) > span").innerText
 
             return registros
         })
@@ -82,3 +67,6 @@ let scrape = async (cnpj) => {
     }
 }
 
+// scrape().then((value) => {
+//     console.log(value)
+// })
